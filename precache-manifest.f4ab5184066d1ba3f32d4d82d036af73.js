662a6912d5015e3e12bf87bc02732e14a8f166b2
macycle <1124942097@qq.com>
2020-08-10 11:29:50 +0800

    deploy

---
+++ precache-manifest.f4ab5184066d1ba3f32d4d82d036af73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11b77434c460b4e6021",
+    "url": "/wallet-website/css/app.e1ff3284.css"
+  },
+  {
+    "revision": "0e21b7972b2152d1556741774de03723",
+    "url": "/wallet-website/index.html"
+  },
+  {
+    "revision": "c11b77434c460b4e6021",
+    "url": "/wallet-website/js/app.e95ab218.js"
+  },
+  {
+    "revision": "e86d160d832b11114ad5",
+    "url": "/wallet-website/js/chunk-vendors.d1953822.js"
+  },
+  {
+    "revision": "3d1efc7c752d19c13ac5b8b665a3f580",
+    "url": "/wallet-website/manifest.json"
+  },
+  {
+    "revision": "c408999ee74390bc06eb49c4fdc513e3",
+    "url": "/wallet-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wallet-website/robots.txt"
+  }
+]);
